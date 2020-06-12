9679c0afc76ea4764b87e04488a8d94394675da5
Juan Salazar <juanzjck1996@gmail.com>
2020-06-12 13:21:48 -0500

    Editar notas

--- src/controllers/notes.controller.js
+++ src/controllers/notes.controller.js
@@ -84,18 +84,76 @@ notesCtrl.renderNotes = async (req, res) => {
 
 notesCtrl.renderEditForm = async (req, res) => {
   const note = await Note.findById(req.params.id);
-  if (note.user != req.user.id) {
-    req.flash("error_msg", "No Autorizado");
-    return res.redirect("/notes");
-  }
-  res.render("notes/edit-note", { note });
+  let outnote={
+    _id:note._id,
+    title:note.title,
+    description:note.description,
+    score:note.score,
+   };
+   let categories=[]
+   new Promise((succes,reject)=>{
+    console.log("start")
+    note.categories.map(async doc=>{
+        const nameCategory= await Category.findById({_id:doc})
+        console.log(nameCategory.name)
+        categories.push(nameCategory.name)
+      })
+     
+      setTimeout(()=>{
+        succes('succes')
+      },1000)
+   }).finally(()=>{
+    console.log(categories)
+    console.log("end")
+    outnote.categories=categories;
+    if (note.user != req.user.id) {
+      req.flash("error_msg", "No Autorizado");
+      return res.redirect("/notes");
+    }
+    res.render("notes/edit-note", { note:outnote });
+  
+   })
+ 
 };
 
 notesCtrl.updateNote = async (req, res) => {
-  const { title, description } = req.body;
-  await Note.findByIdAndUpdate(req.params.id, { title, description });
-  req.flash("success_msg", "Nota actualizada exitosamente");
-  res.redirect("/notes");
+  const { title, description, categories } = req.body;
+  //category
+  const categoriesList=categories.split(",")
+  let outCategories=[]
+  new Promise((responde, reject)=>{
+  
+    categoriesList.map(async doc=>{
+      new Promise(async(response,rejection)=>{
+        let existCategory=  await  Category.findOne({name:doc})
+        response(existCategory)
+      }).then(async(res)=>{
+        if(res===null){
+          let newCategory=new Category({name:doc})
+          newCategory.weight=0;
+          await newCategory.save();
+        } 
+      }).then(async(s)=>{
+        const response=  await  Category.findOne({name:doc})
+        console.log(response._id)
+        outCategories.push(response._id)
+        console.log(`new category created ${response.name}`)
+      }).finally(()=>{
+        setTimeout(()=>{
+          responde(outCategories)
+        },1000)
+      })
+      
+     
+    })
+  }).finally(async (data)=>{
+  
+    await Note.findByIdAndUpdate(req.params.id, { title, description, categories:outCategories });
+    req.flash("success_msg", "Nota actualizada exitosamente");
+    res.redirect("/notes");
+  })
+
+
 };
 
 notesCtrl.deleteNote = async (req, res) => {
