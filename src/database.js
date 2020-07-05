64d510f8bac0b6f7c63ec0cffbbea21d8f9468f5
Juan Salazar <juanzjck1996@gmail.com>
2020-07-05 12:48:25 -0500

    puertos y base de datos

--- src/database.js
+++ src/database.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
 const { NOTES_APP_MONGODB_HOST, NOTES_APP_MONGODB_DATABASE } = process.env;
-
-const MONGODB_URI = `mongodb://localhost/notasDaniel`;
-
+//
+//const MONGODB_URI = `mongodb://localhost/notasDaniel`;
+const MONGODB_URI = process.env.MONGODB_URI || `mongodb+srv://other:TLhpEuLUrtQ1Ylt6@clusteryapa-aaxxm.mongodb.net/socialNotes?retryWrites=true&w=majority`;
 mongoose
   .connect(MONGODB_URI, {
     useNewUrlParser: true,
